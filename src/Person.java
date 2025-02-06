3341e8df59cedeb7b45a1fee986b3bb7298a1ba9
kd <erm.melnik@gmail.com>
2025-02-06 11:00:30 +0100

    Added Equals and Hashcode

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Person {
     private final String name;
     private final int age;
@@ -12,6 +14,21 @@ public class Person {
     }
     public int getAge() {
         return age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(name);
+        result = 31 * result + age;
+        return result;
     }
 
     public String toString(){
